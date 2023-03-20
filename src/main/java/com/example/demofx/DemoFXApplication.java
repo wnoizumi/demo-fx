3370a002555b282953d90232b4dee27a38a8491f
Willian Nalepa Oizumi <oizumi.willian@gmail.com>
2023-03-19 23:17:38 -0300

    removendo arquivos desnecessários e renomeando a aplicacao

--- src/main/java/com/example/demofx/DemoFXApplication.java
+++ src/main/java/com/example/demofx/DemoFXApplication.java
@@ -1,14 +1,11 @@
 package com.example.demofx;
 
-import com.example.demofx.service.LoginService;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class DemoFXApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         LoginController loginController = new LoginController();
